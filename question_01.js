d4e409bbd1b373b758d1034e02fcfe9e6ab27b25
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-15 11:27:10 +0500

    question 1 code added

--- question_01.js
+++ question_01.js
@@ -1,3 +1,14 @@
 // Create a function called `counter` that returns another function. The returned
 // function should increment a counter variable and log its value. Use a closure to keep
 // track of the counter variable.
+function counter(){
+    let counter=0
+    function incrementFunc(){
+        counter++
+        console.log(`counter value is ${counter}`)
+    }
+    return incrementFunc()
+}
+const result=counter()
+
+
