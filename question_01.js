536d12a5cc34b68ba94ec72949a551b23e92bc03
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-15 11:11:03 +0500

    initiall commit

---
+++ question_01.js
@@ -0,0 +1,3 @@
+// Create a function called `counter` that returns another function. The returned
+// function should increment a counter variable and log its value. Use a closure to keep
+// track of the counter variable.
