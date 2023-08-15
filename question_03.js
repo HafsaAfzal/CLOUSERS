10006fdb407c679fd52caf24ee94e0bc22bbac75
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-15 13:04:28 +0500

    initiall commit

---
+++ question_03.js
@@ -0,0 +1,3 @@
+// Create a function called `secretMessage` that takes a string as a parameter. The
+// function should return another function that logs the secret message along with the
+// provided string. Use a closure to access the string parameter.
