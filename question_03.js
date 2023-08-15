81f77b6785c1ce4abcff60ce49d32ac74afbb22f
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-15 13:10:05 +0500

    correct code of question 3 added

--- question_03.js
+++ question_03.js
@@ -1,3 +1,11 @@
 // Create a function called `secretMessage` that takes a string as a parameter. The
 // function should return another function that logs the secret message along with the
 // provided string. Use a closure to access the string parameter.
+function secretMessage(string){
+    function logs(){
+        console.log(`successfuly logs the ${string}`)
+    }
+    return logs
+}
+let s=secretMessage(`secret message from another function`)
+s()
