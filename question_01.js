388a53c45f9ab339d9c88a9b0cada8321ed2bf41
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-15 13:01:50 +0500

    correct and commenting code of question 1 added

--- question_01.js
+++ question_01.js
@@ -4,11 +4,15 @@
 function counter(){
     let counter=0
     function incrementFunc(){
+        // let counter=0
         counter++
         console.log(`counter value is ${counter}`)
     }
-    return incrementFunc()
+    return incrementFunc;  //this reeturn the inrement function
 }
-const result=counter()
+
+const  result =counter() ;  // variable result contain the return value of counter function that is increment function
+// console.log(result)     //output is undefined because result contain increment function return but this fuction is not call nor execute
+result()    //
 
 
